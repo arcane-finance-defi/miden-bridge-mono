58cb6fd4578696d8af4573df1eb48251fdbc931c
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2025-04-22 15:34:59 +0300

    feat: crosschain note constructor

---
+++ miden/src/notes/crosschain.rs
@@ -0,0 +1,49 @@
+use miden_objects::{account::AccountId, asset::FungibleAsset, note::{Note, NoteAssets, NoteExecutionHint, NoteExecutionMode, NoteInputs, NoteMetadata, NoteRecipient, NoteTag, NoteType}, Felt, FieldElement, NoteError, Word};
+
+use super::bridge::croschain;
+
+pub fn new_crosschain_note(
+    serial_number: Word,
+    output_serial_number: Word,
+    dest_chain: Felt,
+    dest_addr: [Felt; 3],
+    faucet_id: AccountId,
+    asset_amount: u64
+) -> Result<Note, NoteError> {
+    let note = Note::new(
+        NoteAssets::new(
+            vec![
+                FungibleAsset::new(faucet_id, asset_amount)
+                    .map_err(|e| NoteError::AddFungibleAssetBalanceError(e))?
+                    .into()
+            ]
+        )?, 
+        NoteMetadata::new(
+            faucet_id, 
+            NoteType::Private, 
+            NoteTag::from_account_id(faucet_id, NoteExecutionMode::Local)?, 
+            NoteExecutionHint::always(), 
+            Felt::ZERO
+        )?, 
+        NoteRecipient::new(
+            serial_number, 
+            croschain(), 
+            NoteInputs::new(vec![
+                output_serial_number[3],
+                output_serial_number[2],
+                output_serial_number[1],
+                output_serial_number[0],
+                dest_addr[2],
+                dest_addr[1],
+                dest_addr[0],
+                dest_chain,
+                Felt::ZERO,
+                Felt::ZERO,
+                Felt::ZERO,
+                Felt::ZERO,
+            ])?
+        )
+    );
+
+    Ok(note)
+}
