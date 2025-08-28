eca260bf3a34d66fb19bed5daf7e368df282432c
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2025-08-28 18:46:08 +0300

    feat: timelocked crosschain funds

--- miden/tests/account/wrapper.rs
+++ miden/tests/account/wrapper.rs
@@ -13,6 +13,8 @@ use miden_testing::{AccountState, Auth, MockChain};
 use miden_bridge::accounts::{token_wrapper::bridge_note_tag, testing::create_token_wrapper_account_builder};
 use miden_bridge::notes::bridge::{bridge, croschain};
 use miden_lib::account::faucets::BasicFungibleFaucet;
+use miden_bridge::errors::note_errors::ERR_CROSSCHAIN_TOO_EARLY_EXECUTION;
+use crate::assert_transaction_executor_error;
 
 pub fn get_new_pk_and_authenticator(seed: [Felt; 4]) -> (PublicKey, AuthSecretKey) {
     let seed = Word::from(seed);
@@ -23,6 +25,8 @@ pub fn get_new_pk_and_authenticator(seed: [Felt; 4]) -> (PublicKey, AuthSecretKe
         AuthSecretKey::RpoFalcon512(sec_key),
     )
 }
+
+const DAY: u32 = 60 * 60 * 24;
 
 #[test]
 fn should_issue_public_bridge_note() -> anyhow::Result<()> {
@@ -88,6 +92,7 @@ fn should_issue_public_bridge_note() -> anyhow::Result<()> {
     ];
 
     let chain_id: u64 = 123;
+    let unlock_timestamp = mock_chain.latest_block_header().timestamp() + 7 * DAY;
 
     let note_inputs = NoteInputs::new(vec![
         output_serial_num[0],
@@ -98,6 +103,7 @@ fn should_issue_public_bridge_note() -> anyhow::Result<()> {
         receiver_address[0],
         receiver_address[1],
         receiver_address[2],
+        Felt::new(unlock_timestamp as u64),
         Felt::ZERO,
         call_address[0],
         call_address[1],
@@ -121,7 +127,7 @@ fn should_issue_public_bridge_note() -> anyhow::Result<()> {
     );
 
     mock_chain.add_pending_note(OutputNote::Full(note.clone()));
-    mock_chain.prove_next_block();
+    mock_chain.prove_next_block().expect("Unable to prove next block");
 
     let mint_tx_inputs = mock_chain.get_transaction_inputs(
         wrapper.clone(),
@@ -160,8 +166,7 @@ fn should_issue_public_bridge_note() -> anyhow::Result<()> {
     );
 
     let mint_tx_script =
-        TransactionScript::compile(mint_tx_script_code, TransactionKernel::testing_assembler())
-            ?;
+        TransactionScript::compile(mint_tx_script_code, TransactionKernel::testing_assembler())?;
 
     let executed_mint_transaction = mock_chain.build_tx_context(
             wrapper.clone(),
@@ -171,8 +176,7 @@ fn should_issue_public_bridge_note() -> anyhow::Result<()> {
             .tx_script(mint_tx_script)
             .tx_inputs(mint_tx_inputs)
             .build()?
-            .execute()?;
-
+            .execute().expect("Unable to execute mint tx");
 
     mock_chain.add_pending_executed_transaction(&executed_mint_transaction.clone())?;
     mock_chain.prove_next_block()?;
@@ -226,15 +230,42 @@ fn should_issue_public_bridge_note() -> anyhow::Result<()> {
         )?;
 
 
-    let executed_transaction = mock_chain.build_tx_context(
+    let failed_executed_transaction = mock_chain.build_tx_context(
             wrapper.clone(),
             &[],
             &[]
         )?
             .tx_inputs(tx_inputs)
-            .extend_expected_output_notes(vec![OutputNote::Full(expected_note)])
+            .extend_expected_output_notes(vec![OutputNote::Full(expected_note.clone())])
             .build()?
-            .execute()?;
+            .execute();
+
+    assert_transaction_executor_error!(
+        failed_executed_transaction,
+        ERR_CROSSCHAIN_TOO_EARLY_EXECUTION
+    );
+
+    mock_chain.prove_next_block_at(unlock_timestamp + DAY)
+        .expect("Unable to generate next block");
+
+
+    let tx_inputs = mock_chain.
+        get_transaction_inputs(
+            wrapper.clone(),
+            None,
+            &[note.clone().id()],
+            &[]
+        )?;
+
+    let executed_transaction = mock_chain.build_tx_context(
+        wrapper.clone(),
+        &[],
+        &[]
+    )?
+        .tx_inputs(tx_inputs.clone())
+        .extend_expected_output_notes(vec![OutputNote::Full(expected_note.clone())])
+        .build()?
+        .execute().expect("Unable to execute crosschain consume transaction");
 
     assert_eq!(executed_transaction.output_notes().num_notes(), 1);
     assert_eq!(executed_transaction.output_notes().get_note(0).metadata().tag(), bridge_note_tag());
